21c2e1d00c78100788155bc3431e5694926a0d36
Julian So <ch.julian.so@gmail.com>
2021-03-07 18:35:16 -0500

    work further on OOP

--- practice/main.js
+++ practice/main.js
@@ -4,17 +4,67 @@ let rate = 20;
 
 // Procedural
 function getWage(baseSalary, overtime, rate) {
-  return baseSalary + (overtime * rate);
-};
+  return baseSalary + overtime * rate;
+}
 
 // OOP
 let employee = {
   baseSalary: 30000,
   overtime: 10,
   rate: 20,
-  getWage: function() {
-    return this.baseSalary + (this.overtime * this.rate);
-  }
+  getWage: function () {
+    return this.baseSalary + this.overtime * this.rate;
+  },
 };
 
 employee.getWage();
+
+// circle example
+
+let circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  draw: () => {
+    console.log("draw");
+  },
+};
+
+circle.draw();
+
+
+// Constructor Function
+function Circle(radius) {
+  this.radius = radius;
+  
+  let defaultLocation = { x: 0, y: 0};
+
+  this.getDefaultLocation = function() {
+    return defaultLocation;
+  }
+
+  this.draw = function () {
+    console.log("draw");
+  };
+
+  Object.defineProperty() {
+    
+  }
+}
+
+const newCircle = new Circle(10);
+circle.draw();
+
+for (let key in circle) {
+  if (typeof circle[key] !== 'function') {
+    console.log(key, circle[key]);
+  }
+}
+
+const keys = Object.keys(circle);
+newCircle.getDefaultLocation();
+
+
+ 
