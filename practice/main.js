186ab715ccb51a84017eb3c873de9a7a24e33879
Julian So <ch.julian.so@gmail.com>
2021-03-07 12:26:45 -0500

    learning OOP

--- practice/main.js
+++ practice/main.js
@@ -1,5 +1,20 @@
-function Stopwatch() {
-  // 58:21
-}
+let baseSalary = 30000;
+let overtime = 10;
+let rate = 20;
 
-const sw = new Stopwatch();
+// Procedural
+function getWage(baseSalary, overtime, rate) {
+  return baseSalary + (overtime * rate);
+};
+
+// OOP
+let employee = {
+  baseSalary: 30000,
+  overtime: 10,
+  rate: 20,
+  getWage: function() {
+    return this.baseSalary + (this.overtime * this.rate);
+  }
+};
+
+employee.getWage();
