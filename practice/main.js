5039da12bd327d37340d1c24fc9029a93fd421f9
Julian So <ch.julian.so@gmail.com>
2021-03-07 21:18:23 -0500

    did stopwatch exercise Mosh

--- practice/main.js
+++ practice/main.js
@@ -32,39 +32,83 @@ let circle = {
   },
 };
 
-circle.draw();
-
+// circle.draw();
 
 // Constructor Function
 function Circle(radius) {
   this.radius = radius;
-  
-  let defaultLocation = { x: 0, y: 0};
 
-  this.getDefaultLocation = function() {
+  let defaultLocation = { x: 0, y: 0 };
+
+  this.getDefaultLocation = function () {
     return defaultLocation;
-  }
+  };
 
   this.draw = function () {
     console.log("draw");
   };
 
-  Object.defineProperty() {
-    
-  }
+  Object.defineProperty(this, "defaultLocation", {});
 }
 
 const newCircle = new Circle(10);
-circle.draw();
 
-for (let key in circle) {
-  if (typeof circle[key] !== 'function') {
-    console.log(key, circle[key]);
-  }
-}
+// for (let key in circle) {
+//   if (typeof circle[key] !== 'function') {
+//     console.log(key, circle[key]);
+//   }
+// }
 
 const keys = Object.keys(circle);
 newCircle.getDefaultLocation();
 
+///
 
- 
+function Stopwatch() {
+  let startTime = 0;
+  let endTime = 0;
+  let running = 0;
+  let duration = 0;
+
+  this.start = function () {
+    if (!running) {
+      running = true;
+      startTime = Date.now();
+    } else {
+      throw new Error("Stopwatch has already started!");
+    }
+  };
+
+  this.stop = function () {
+    if (running) {
+      running = false;
+      endTime = Date.now();
+      duration = (endTime - startTime) / 1000;
+    } else {
+      throw new Error("Stopwatch has already stopped!");
+    }
+  };
+
+  this.reset = function () {
+    startTime = 0;
+    endTime = 0;
+    running = false;
+    duration = 0;
+    return;
+  };
+
+  Object.defineProperty(this, "duration", {
+    get: function () {
+      return duration
+      // return (duration = (endTime - startTime) / 1000);
+    },
+    set: function (value) {
+      if (!value.duration) {
+        throw new Error("");
+      }
+      duration = value;
+    },
+  });
+}
+
+const sw = new Stopwatch();
