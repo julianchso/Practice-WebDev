57f5a58b2cbb16bfd266b4fdcf658757180ab757
Julian So <ch.julian.so@gmail.com>
2021-03-27 16:09:05 -0400

    learning api

--- practice/api.js
+++ practice/api.js
@@ -1,1 +1,14 @@
+document.querySelector("button").addEventListener("click", getStockInfo);
 
+function getStockInfo() {
+  let ticker = document.querySelector("input").value;
+  let apikey = "TOY6Q1GZESECU7VJ";
+  const url = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${ticker}&apikey=${apikey}`;
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      document.querySelector("h2").innerText = data.Name
+    })
+}
