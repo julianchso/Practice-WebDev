d4f88ab58e02a22be785c910b36db70d472e4295
Julian So <ch.julian.so@gmail.com>
2021-03-28 20:10:32 -0400

    more api learning

--- practice/api.js
+++ practice/api.js
@@ -1,14 +1,31 @@
+// document.querySelector("button").addEventListener("click", getStockInfo);
+
+// function getStockInfo() {
+//   let ticker = document.querySelector("input").value;
+//   let apikey = "TOY6Q1GZESECU7VJ";
+  
+//   const url = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${ticker}&apikey=${apikey}`;
+
+//   fetch(url)
+//     .then(res => res.json())
+//     .then(data => {
+//       console.log(data);
+//       document.querySelector("h2").innerText = data.Name
+//     })
+// }
+
 document.querySelector("button").addEventListener("click", getStockInfo);
 
 function getStockInfo() {
   let ticker = document.querySelector("input").value;
   let apikey = "TOY6Q1GZESECU7VJ";
+
   const url = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${ticker}&apikey=${apikey}`;
 
   fetch(url)
-    .then(res => res.json())
-    .then(data => {
+    .then((res) => res.json())
+    .then((data) => {
       console.log(data);
-      document.querySelector("h2").innerText = data.Name
+      document.querySelector("h2").innerText = data.Name;
     })
 }
