fd76e89a8dc7efca2672218b5fb4161418b02605
Julian So <ch.julian.so@gmail.com>
2021-04-03 15:03:32 -0400

    Review constructors

--- practice/main.js
+++ practice/main.js
@@ -19,18 +19,18 @@
 // console.log("Sum=" + calculator.sum());
 // console.log("Mul=" + calculator.mul());
 
-function Accumulator(startingValue) {
-  this.value = startingValue;
-  this.read = function () {
-    this.value += Number(prompt("Number?"));
-  };
+// function Accumulator(startingValue) {
+//   this.value = startingValue;
+//   this.read = function () {
+//     this.value += Number(prompt("Number?"));
+//   };
 
-  return this.value;
-}
+//   return this.value;
+// }
 
-let accumulator = new Accumulator(1); // initial value 1
+// let accumulator = new Accumulator(1); // initial value 1
 
-accumulator.read(); // adds the user-entered value
-accumulator.read(); // adds the user-entered value
+// accumulator.read(); // adds the user-entered value
+// accumulator.read(); // adds the user-entered value
 
-console.log(accumulator.value);
+// console.log(accumulator.value);
