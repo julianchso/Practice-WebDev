d42a3cac97a90738a5a774860251eff9fef5d341
Julian So <ch.julian.so@gmail.com>
2021-04-17 18:17:54 -0400

    work on getting response of pokeapi type

--- Learn-API/rainbow-badge-pokemon/pokeapi.js
+++ Learn-API/rainbow-badge-pokemon/pokeapi.js
@@ -5,6 +5,8 @@ document.querySelector("button").addEventListener("click", clear);
 let pokeImg = [];
 let pokeType1 = [];
 let pokeType2 = [];
+let pokeType1Url = [];
+let pokeType2Url = [];
 
 function getFetch() {
   const poke1 = document.querySelector("#poke1").value;
@@ -19,35 +21,40 @@ function getFetch() {
       pokeImg.push(data.sprites.front_default);
       data.types.forEach((e) => {
         pokeType1.push(e.type.name);
+        pokeType1Url.push(e.type.url);
       });
-      console.log(pokeType1);
 
       pokeType1.push();
       document.querySelector("#pokeImg1").src = pokeImg[0];
       document.querySelector("#type1").innerHTML = pokeType1;
-      
+      console.log(pokeType1);
+
       // console.log(pokeImg)
       console.log(data);
       // console.log(data.types[0].type.name);
-      
-      fetch(url2)
-      .then((res) => res.json())
-      .then((data) => {
-        pokeImg.push(data.sprites.front_default);
-        data.types.forEach((e) => {
-          pokeType2.push(e.type.name);
-        });
-        console.log(pokeType2);
-        
-        document.querySelector("#pokeImg2").src = pokeImg[1];
-        document.querySelector("#type2").innerHTML = pokeType2;
-        });
-      // fetch image and type of pokemon
-        
-      // fetch type to decide if it's super effective
-    })
 
-  
+      fetch(url2)
+        .then((res) => res.json())
+        .then((data) => {
+          pokeImg.push(data.sprites.front_default);
+          data.types.forEach((e) => {
+            pokeType2.push(e.type.name);
+            pokeType2Url.push(e.type.url);
+          });
+
+          document.querySelector("#pokeImg2").src = pokeImg[1];
+          document.querySelector("#type2").innerHTML = pokeType2;
+          console.log(pokeType2);
+        });
+      console.log(pokeType1Url);
+      return fetch(pokeType1Url[0]);
+    })
+    .then((res) => res.json())
+    .then((typeData) => {
+      console.log(typeData);
+    });
+  console.log(pokeType1);
+  console.log(pokeType2);
 }
 
 function clear() {
