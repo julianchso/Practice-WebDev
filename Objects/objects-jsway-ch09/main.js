0bd0edfbffcb3a92a6333e800b3605dbd905bdef
Julian So <ch.julian.so@gmail.com>
2021-04-18 15:48:50 -0400

    finish bank account exercise

--- Objects/objects-jsway-ch09/main.js
+++ Objects/objects-jsway-ch09/main.js
@@ -24,69 +24,101 @@
 // console.log(`${snowy.name} is a ${snowy.breed} dog measuring ${snowy.size}`);
 // console.log(`Look, a cat! ${snowy.name} barks: ${snowy.bark()}`);
 
-// Improve the example RPG to add character inventory management according to the following rules:
-// A character's inventory contains a number of gold and a number of keys.
-// Each character begins with 10 gold and 1 key.
-// The character description must show the inventory state.
-// When a character slays another character, the victim's inventory goes to its vanquisher.
+// // Improve the example RPG to add character inventory management according to the following rules:
+// // A character's inventory contains a number of gold and a number of keys.
+// // Each character begins with 10 gold and 1 key.
+// // The character description must show the inventory state.
+// // When a character slays another character, the victim's inventory goes to its vanquisher.
 
-class Character {
-  constructor(name, health, strength, inventory) {
+// class Character {
+//   constructor(name, health, strength, inventory) {
+//     this.name = name;
+//     this.health = health;
+//     this.strength = strength;
+//     this.xp = 0;
+//     this.inventory = {
+//       gold: 10,
+//       key: 1,
+//     };
+//   }
+
+//   attack(target) {
+//     if (this.health > 0) {
+//       const damage = this.strength;
+//       console.log(`${this.name} attacks ${target.name} for ${damage} damage.`);
+
+//       target.health -= damage;
+//       if (target.health > 0) {
+//         console.log(`${target.name} now has ${target.health} hp left.`);
+//       } else {
+//         target.health = 0;
+//         const bonusXP = 10;
+//         console.log(`${this.name} has killed ${target.name}.`);
+//         this.xp += 10;
+
+//         Object.keys(target.inventory).forEach((key) => {
+//           if (key in this.inventory) {
+//             this.inventory[key] += target.inventory[key];
+//           }
+//         });
+//       }
+//     } else {
+//       console.log(`${this.name} cannot attack because they've been killed.`);
+//     }
+//   }
+
+//   describe() {
+//     return `${this.name} has ${this.health} health points, ${this.strength} strength,
+//     ${this.xp} xp, and inventory containing ${this.inventory.gold} gold and ${this.inventory.key} key(s)`;
+//   }
+// }
+
+// const aurora = new Character("Aurora", 150, 25, 0);
+// const glacius = new Character("Glacius", 130, 30, 0);
+
+// console.log("Welcome to the adventure! Here are our heroes:");
+// console.log(aurora.describe());
+// console.log(glacius.describe());
+
+// const monster = new Character("Spike", 40, 20);
+// console.log("A wild monster has appeared: it's named " + monster.name);
+
+// monster.attack(aurora);
+// monster.attack(glacius);
+// aurora.attack(monster);
+// glacius.attack(monster);
+
+// console.log(aurora.describe());
+// console.log(glacius.describe());
+
+// console.log(glacius);
+
+// Write a program that creates bank accounts
+
+class BankAccount {
+  constructor(name) {
     this.name = name;
-    this.health = health;
-    this.strength = strength;
-    this.xp = 0;
-    this.inventory = {
-      gold: 10,
-      key: 1,
-    };
+    this.balance = 0;
   }
 
-  attack(target) {
-    if (this.health > 0) {
-      const damage = this.strength;
-      console.log(`${this.name} attacks ${target.name} for ${damage} damage.`);
-
-      target.health -= damage;
-      if (target.health > 0) {
-        console.log(`${target.name} now has ${target.health} hp left.`);
-      } else {
-        target.health = 0;
-        const bonusXP = 10;
-        console.log(`${this.name} has killed ${target.name}.`);
-        this.xp += 10;
-
-        Object.keys(target.inventory).forEach((key) => {
-          if (key in this.inventory) {
-            this.inventory[key] += target.inventory[key];
-          }
-        });
-      }
-    } else {
-      console.log(`${this.name} cannot attack because they've been killed.`);
-    }
+  credit(amount) {
+    this.balance += amount;
+    return this.balance;
   }
 
   describe() {
-    return `${this.name} has ${this.health} health points, ${this.strength} strength,
-    ${this.xp} xp, and inventory containing ${this.inventory.gold} gold and ${this.inventory.key} key(s)`;
+    return `owner: ${this.name}, balance: ${this.balance}`
   }
 }
 
-const aurora = new Character("Aurora", 150, 25, 0);
-const glacius = new Character("Glacius", 130, 30, 0);
+const BankAccounts = [];
 
-console.log("Welcome to the adventure! Here are our heroes:");
-console.log(aurora.describe());
-console.log(glacius.describe());
+BankAccounts.push(new BankAccount("Alex"));
+BankAccounts.push(new BankAccount("Brad"));
+BankAccounts.push(new BankAccount("Georges"));
 
-const monster = new Character("Spike", 40, 20);
-console.log("A wild monster has appeared: it's named " + monster.name);
+BankAccounts.forEach(account => {
+  account.credit(1000)
+});
 
-monster.attack(aurora);
-monster.attack(glacius);
-aurora.attack(monster);
-glacius.attack(monster);
-
-console.log(aurora.describe());
-console.log(glacius.describe());
+console.log(BankAccounts)
