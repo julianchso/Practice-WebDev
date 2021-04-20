44930e0e2ef02b0fb307543135c49e7a07d5c72d
Julian So <ch.julian.so@gmail.com>
2021-04-19 22:33:10 -0400

    minor change to OOP exercise

--- Objects/objects-espresso-machine/js/main.js
+++ Objects/objects-espresso-machine/js/main.js
@@ -1,1 +1,24 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+
+class EspressoMachine {
+  constructor(colour, make, model, price) {
+    this.colour = colour;
+    this.make = make;
+    this.model = model;
+    this.price = price;
+  }
+
+  input() {
+    console.log("press buttons");
+  }
+
+  steam() {
+    console.log("steamy...");
+  }
+
+  start() {
+    console.log("making drink");
+  }
+}
+
+let gaggia = new EspressoMachine("red", "gaggia", "3000", 999);
