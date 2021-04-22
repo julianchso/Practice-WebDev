1fe0cd6a396e76647f4a58ed3735106cba24941b
Julian So <ch.julian.so@gmail.com>
2021-04-21 22:07:24 -0400

    practice on classes and subclasses

--- Objects/objects-child-class/js/main.js
+++ Objects/objects-child-class/js/main.js
@@ -2,9 +2,13 @@
 
 class fighter {
   constructor(name, universe, powerLevel) {
-    this.name = name;
+    this._name = name;
     this.universe = universe;
     this.powerLevel = powerLevel;
+  }
+  get name() {
+    // underscore means a variable is private. Do not change it.
+    return this._name;
   }
 }
 
@@ -20,3 +24,31 @@ class goku extends fighter {
 }
 
 goku1 = new goku("goku", "dragonBall", "9000");
+
+class Animal {
+  constructor(name) {
+    this._name = name;
+  }
+  get name() {
+    return this._name;
+  }
+  speak() {
+    console.log(`${this._name} makes a sound.`);
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, breed) {
+    super(name);
+    this._breed = breed;
+  }
+  get breed() {
+    return this._breed;
+  }
+  speak() {
+    super.speak()
+    console.log(`${this._name} barks.`)
+  }
+}
+
+let simba = new Dog("Simba","Shiba Inu");
