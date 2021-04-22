1bd4153cb9fd0df719e1f1924adf6fb2ca5af978
Julian So <ch.julian.so@gmail.com>
2021-04-21 20:41:28 -0400

    play around with extended classes

--- Objects/objects-child-class/js/main.js
+++ Objects/objects-child-class/js/main.js
@@ -1,2 +1,22 @@
-//Create an a class and extend it - Can be anything you would like it to be! 
+//Create an a class and extend it - Can be anything you would like it to be!
 
+class fighter {
+  constructor(name, universe, powerLevel) {
+    this.name = name;
+    this.universe = universe;
+    this.powerLevel = powerLevel;
+  }
+}
+
+class goku extends fighter {
+  constructor(name, universe, powerLevel) {
+    super(name, universe, powerLevel);
+    this.specialAbilities = [];
+  }
+
+  addAbility(ability) {
+    this.specialAbilities.push(ability);
+  }
+}
+
+goku1 = new goku("goku", "dragonBall", "9000");
