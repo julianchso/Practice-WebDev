4a3372095af9778eebb8a41a328ed12c97e9a41d
Julian So <ch.julian.so@gmail.com>
2021-05-22 15:18:38 -0400

    learning node basics

--- practice.js
+++ practice.js
@@ -1,63 +1,44 @@
-// // Answer
+const fs = require("fs");
+const path = require("path");
+const os = require("os");
 
-// class HttpError extends Error {
-//   constructor(response) {
-//     super(`${response.status} for ${response.url}`);
-//     this.name = "HttpError";
-//     this.response = response;
+// fs.mkdir(path.join(__dirname, "/test"), {}, (err) => {
+//   if (err) throw err;
+//   console.log("Folder created...");
+// });
+
+// fs.writeFile(
+//   path.join(__dirname, "/test", "hello.txt"),
+//   "Hello world",
+//   (err) => {
+//     if (err) throw err;
+//     console.log("File created...");
 //   }
-// }
+// );
 
-// async function loadJson(url) {
-//   let response = await fetch(url);
-
-//   if (response.status == 200) {
-//     return response.json();
-//   } else {
-//     throw new HttpError(response);
-//   }
-// }
-
-// // Ask for a user name until github returns a valid user
-// async function demoGithubUser() {
-//   let user;
-
-//   while (true) {
-//     let name = prompt("Enter a name?", "iliakan");
-
-//     try {
-//       user = await loadJson(`https://api.github.com/users/${name}`);
-//       break;
-//     } catch {
-//       if (err instanceof HttpError && err.response.status == 404) {
-//         alert("No such user, please reenter.");
-//       } else {
-//         throw err;
+// fs.writeFile(
+//   path.join(__dirname, "/test", "hello.txt"),
+//   "I love Node.js",
+//   (err) => {
+//     if (err) throw err;
+//     console.log("File written to...");
+//     fs.appendFile(
+//       path.join(__dirname, "/test", "hello.txt"),
+//       " Hello world",
+//       (err) => {
+//         if (err) throw err;
+//         // console.log("File written to...");
 //       }
-//     }
-//     alert(`Full name: ${user.name}.`);
-//     return user;
+//     );
 //   }
-// }
+// );
 
-// demoGithubUser();
+// fs.readFile(path.join(__dirname, "/test", "hello.txt"), "utf8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
 
-// Call async from non-async
+// console.log(os.arch());
+// console.log(os.constants);
+console.log(os.cpus());
 
-async function wait() {
-  await new Promise(resolve => setTimeout(resolve, 1000));
-
-  return 10;
-}
-
-function f() {
-
-  wait()
-    .then(result => console.log(result))
-
-  // ...what should you write here?
-  // we need to call async wait() and wait to get 10
-  // remember, we can't use "await"
-}
-
-console.log(f());
