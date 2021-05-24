b0b389da135439339a2551024e0e1504218308bf
Julian So <ch.julian.so@gmail.com>
2021-05-23 22:53:05 -0400

    100Dev, learning about promises async await

--- Async&Promise/async-practice/js/main.js
+++ Async&Promise/async-practice/js/main.js
@@ -1,4 +1,4 @@
-//Code 01
+// Code 01
 // function houseOne(){
 //     console.log('Paper delivered to house 1')
 // }
@@ -26,121 +26,129 @@
 // houseTwo();
 // houseThree();
 
-//Code 03
-// function houseOne(){
-//     console.log('Paper delivered to house 1')
+// Code 03
+// function houseOne() {
+//   console.log("Paper delivered to house 1");
 // }
-// function houseTwo(callback){
-//     setTimeout(() => {
-//         console.log('Paper delivered to house 2')
-//         callback()
-//     }, 3000)
+// function houseTwo(callback) {
+//   setTimeout(() => {
+//     console.log("Paper delivered to house 2");
+//     callback();
+//   }, 3000);
 // }
-// function houseThree(){
-//     console.log('Paper delivered to house 3')
+// function houseThree() {
+//   console.log("Paper delivered to house 3");
 // }
-// houseOne()
-// houseTwo(houseThree)
+// houseOne();
+// houseTwo(houseThree);
 
-//Code 04
-// function houseOne(){
+// Code 04
+// function houseOne() {
+//   setTimeout(() => {
+//     console.log("Paper delivered to house 1");
 //     setTimeout(() => {
-//         console.log('Paper delivered to house 1')
-//         setTimeout(() => {
-//             console.log('Paper delivered to house 2')
-//             setTimeout(() => {
-//                 console.log('Paper delivered to house 3')
-//             }, 3000)
-//         }, 4000)
-//     }, 5000)
+//       console.log("Paper delivered to house 2");
+//       setTimeout(() => {
+//         console.log("Paper delivered to house 3");
+//       }, 3000);
+//     }, 4000);
+//   }, 5000);
 // }
-// houseOne()
+// houseOne();
 
-//Code 05
+// Code 05
 // const promise = new Promise((resolve, reject) => {
-//     const error = false
-//     if(!error){
-//         resolve('Promise has been fullfilled')
-//     }else{
-//         reject('Error: Operation has failed')
-//     }
-// })
-// console.log(promise)
-// promise
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err))
+//   const error = false;
+//   if (!error) {
+//     resolve("Promise has been fullfilled");
+//   } else {
+//     reject("Error: Operation has failed");
+//   }
+// });
+// console.log(promise);
+// promise.then((data) => console.log(data)).catch((err) => console.log(err));
 
-//Code 06
-// function houseOne(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Paper delivered to house 1')
-//         }, 1000)
-//     })
+// const promise = new Promise((resolve, reject) => {
+//   const error = false;
+//   if (!error) {
+//     resolve("Promise has been fulfilled");
+//   } else {
+//     reject("Error: Operation has failed")
+//   }
+// })
+
+// Code 06
+// function houseOne() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("Paper delivered to house 1");
+//     }, 1000);
+//   });
 // }
-// function houseTwo(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Paper delivered to house 2')
-//         }, 5000)
-//     })
+// function houseTwo() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("Paper delivered to house 2");
+//     }, 5000);
+//   });
 // }
-// function houseThree(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Paper delivered to house 3')
-//         }, 2000)
-//     })
+// function houseThree() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("Paper delivered to house 3");
+//     }, 2000);
+//   });
 // }
 // houseOne()
-//     .then(data => console.log(data))
-//     .then(houseTwo)
-//     .then(data => console.log(data))
-//     .then(houseThree)
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err))
+//   .then((data) => console.log(data))
+//   .then(houseTwo)
+//   .then((data) => console.log(data))
+//   .then(houseThree)
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
 
-//Code 07
-// function houseOne(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Paper delivered to house 1')
-//         }, 1000)
-//     })
-// }
-// function houseTwo(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Paper delivered to house 2')
-//         }, 5000)
-//     })
-// }
-// function houseThree(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Paper delivered to house 3')
-//         }, 2000)
-//     })
-// }
+// Code 07
+// Makes asynchronous code look synchronous
+function houseOne() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Paper delivered to house 1");
+    }, 1000);
+  });
+}
+function houseTwo() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Paper delivered to house 2");
+    }, 1000);
+  });
+}
+function houseThree() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Paper delivered to house 3");
+    }, 1000);
+  });
+}
 
-// async function getPaid(){
-//     const houseOneWait = await houseOne()
-//     const houseTwoWait = await houseTwo()
-//     const houseThreeWait = await houseThree()
-//     console.log(houseOneWait)
-//     console.log(houseTwoWait)
-//     console.log(houseThreeWait)
-// }
+async function getPaid() {
+  const houseOneWait = await houseOne();
+  const houseTwoWait = await houseTwo();
+  const houseThreeWait = await houseThree();
+  console.log(houseOneWait);
+  console.log(houseTwoWait);
+  console.log(houseThreeWait);
+}
 
-// getPaid()
+getPaid();
 
-//Code 08
-// async function getACuteDogPhoto(){
-//     const res = await fetch('https://dog.ceo/api/breeds/image/random')
-//     const data = await res.json()
-//     console.log(data)
+// Code 08
+// async function getACuteDogPhoto() {
+//   const res = await fetch("https://dog.ceo/api/breeds/image/random");
+//   const data = await res.json();
+//   console.log(data);
 // }
-// getACuteDogPhoto()
+// getACuteDogPhoto();
 
 // // using then
 
@@ -162,111 +170,3 @@
 //       "There has been a problem with your fetch operation: " + e.message
 //     );
 //   });
-
-// // using async await
-
-// console.log('Synchronous 1');
-
-// setTimeout(_ => console.log(`Timeout 2`), 0);
-
-// Promise.resolve().then(_ => console.log('Promise'));
-
-// console.log('Synchronous 4 ');
-
-// const promise = fetch("https://jsonplaceholder.typicode.com/todos/1");
-
-// promise
-//   .then((res) => res.json())
-//   .then((user) => console.log(":D", user.title))
-//   .catch((err) => console.error(err));
-
-// console.log("Synchronous");
-
-const tick = Date.now();
-const log = (v) => console.log(`${v} \n Elapsed: ${Date.now() - tick}`);
-
-const codeBlocker = () => {
-  let i = 0;
-  while (i < 1000000000) {
-    i++;
-  }
-
-  return " ";
-};
-
-// // log("Synchronous 1");
-// // log("while loop", codeBlocker());
-// // log("Synchronous 2");
-
-// const codeBlocker2 = () => {
-//   return new Promise((resolve, reject) => {
-//     let i = 0;
-//     while (i < 1000000000) {
-//       i++;
-//     }
-//     resolve("billion loops done");
-//   });
-// };
-
-// // log("Synchronous 1");
-// // codeBlocker2().then(log);
-// // log("Synchronous 2");
-
-// const codeBlocker3 = () => {
-//   return Promise.resolve().then((v) => {
-//     let i = 0;
-//     while (i < 1000000000) {
-//       i++;
-//     }
-//     return "billion loops done";
-//   });
-// };
-
-// log("Synchronous 1");
-// codeBlocker3().then(log);
-// log("Synchronous 2");
-
-function promiseHell() {
-  let userId;
-  let postId;
-  let db;
-
-  db.then((u) => {
-    return db.user().then((v) => v.json());
-  })
-    .then((u) => {
-      userId = u.id;
-      return db.post(userId).then((v) => v.json());
-    })
-    .then((p) => {
-      postId = p.id;
-      return db.comments(postId).then((v) => v.json());
-    });
-}
-
-function delay(n) {
-  return setTimeout("", n);
-}
-
-const getFruit = async (name) => {
-  const fruits = {
-    pineapple: "pineapple",
-    peach: "peach",
-    strawberry: "strawberry",
-  };
-  await delay(1000);
-
-  return fruits[name];
-};
-
-getFruit("peach").then(console.log);
-
-
-const makeSmoothie = async() => {
-  const a = await getFruit('pineapple');
-  const b = await getFruit('strawberry');
-
-  return [a, b];
-}
-
-makeSmoothie().then(log);
