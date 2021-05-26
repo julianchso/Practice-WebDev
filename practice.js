9a6f710ac4ca21b91e2fac0eeec9eba7e96b9561
Julian So <ch.julian.so@gmail.com>
2021-05-25 21:28:31 -0400

    learning node js backend

--- practice.js
+++ practice.js
@@ -1,47 +1,11 @@
-fetch("coffee.jpg")
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return response.blob();
+const http = require("http");
+const fs = require("fs");
+http
+  .createServer((req, res) => {
+    fs.readFile("practice.html", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
   })
-  .then((myBlob) => {
-    let objectURL = URL.createObjectURL(myBlob);
-    let image = document.createElement("img");
-    image.src = objectURL;
-    document.body.appendChild(image);
-  })
-  .catch((e) => {
-    console.log(
-      "There has been a problem with your fetch operation: " + e.message
-    );
-  });
-
-async function myFetch() {
-  let response = await fetch("coffee.jpg");
-
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-
-  let myBlob = await response.blob();
-
-  let objectURL = URL.createObjectURL(myBlob);
-  let image = document.createElement("img");
-  image.src = objectURL;
-  document.body.appendChild(image);
-}
-
-myFetch().catch((e) => {
-  console.log(
-    "There has been a problem with your fetch oepration: " + e.message
-  );
-});
-
-// Learn: JS All Lies
-// Learn: Call Stack / Task Queue
-// Learn: Event Loop
-// Learn: Node
-// Homework - Coin Flip & RPS
-
-// The event loop monitors the callback queue and the job queue and decides what needs to be pushed to the call stack.
+  .listen(8000);
